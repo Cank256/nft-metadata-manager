09f1ee79ef36b09200f64348d52442305d3cc90d
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-10-13 12:19:57 +0300

    Add Dashboard blocks

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,13 +1,268 @@
 // src/app/dashboard/page.tsx
 import ProtectedRoute from "@/components/ProtectedRoute";
+import Link from "next/link"
+import {
+  Activity,
+  ArrowUpRight,
+  Boxes,
+  Database,
+  Heart,
+} from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function DashboardPage() {
-  return (
-    <ProtectedRoute>
-      <div>
-        <h1>Dashboard</h1>
-        {/* Dashboard content goes here */}
-      </div>
-    </ProtectedRoute>
-  );
-}
+    return (
+        <ProtectedRoute>
+            <div className="flex min-h-screen w-full flex-col">
+                <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+                    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                        <Card x-chunk="dashboard-01-chunk-3">
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">NFTs Uploaded</CardTitle>
+                            <Activity className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">32</div>
+                            <p className="text-xs text-muted-foreground">
+                            +4 since last month
+                            </p>
+                        </CardContent>
+                        </Card>
+                        <Card x-chunk="dashboard-01-chunk-3">
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">NFTs Collections</CardTitle>
+                            <Boxes className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">3</div>
+                            <p className="text-xs text-muted-foreground">
+                            +1 since last month
+                            </p>
+                        </CardContent>
+                        </Card>
+                        <Card x-chunk="dashboard-01-chunk-1">
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                            Favorites
+                            </CardTitle>
+                            <Heart className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">50</div>
+                            <p className="text-xs text-muted-foreground">
+                            +180.1% from last month
+                            </p>
+                        </CardContent>
+                        </Card>
+                        <Card x-chunk="dashboard-01-chunk-2">
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Storage</CardTitle>
+                            <Database className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">1.5 / 5 GB</div>
+                            <p className="text-xs text-muted-foreground">
+                            +19% usage from last month
+                            </p>
+                        </CardContent>
+                        </Card>
+                    </div>
+                    <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+                        <Card
+                        className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
+                        >
+                        <CardHeader className="flex flex-row items-center">
+                            <div className="grid gap-2">
+                            <CardTitle>NFTs Uploaded</CardTitle>
+                            <CardDescription>
+                                Your recent NFT management activities.
+                            </CardDescription>
+                            </div>
+                            <Button asChild size="sm" className="ml-auto gap-1">
+                            <Link href="#">
+                                View All
+                                <ArrowUpRight className="h-4 w-4" />
+                            </Link>
+                            </Button>
+                        </CardHeader>
+                        <CardContent>
+                            <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Item</TableHead>
+                                    <TableHead>Collection</TableHead>
+                                    <TableHead> Action</TableHead>
+                                    <TableHead>Date</TableHead>
+                                    <TableHead>Amount</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Weekend 1.1</TableCell>
+                                    <TableCell>Weekend</TableCell>
+                                    <TableCell>Upload</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Complete</Badge>
+                                    </TableCell>
+                                    <TableCell>05-10-2024</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Weekend 1.0</TableCell>
+                                    <TableCell>Weekend</TableCell>
+                                    <TableCell>Update Meta</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Draft</Badge>
+                                    </TableCell>
+                                    <TableCell>05-10-2024</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Winter 3.2</TableCell>
+                                    <TableCell>Winter</TableCell>
+                                    <TableCell>Upload</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Complete</Badge>
+                                    </TableCell>
+                                    <TableCell>23-09-2024</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Summer 1.4</TableCell>
+                                    <TableCell>Summer</TableCell>
+                                    <TableCell>Upload</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Complete</Badge>
+                                    </TableCell>
+                                    <TableCell>14-09-2024</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Spring 5.0</TableCell>
+                                    <TableCell>Spirng</TableCell>
+                                    <TableCell>Upload</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Complete</Badge>
+                                    </TableCell>
+                                    <TableCell>23-06-2024</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Bearish 4.1</TableCell>
+                                    <TableCell>Bear</TableCell>
+                                    <TableCell>Update Meta</TableCell>
+                                    <TableCell>
+                                        <Badge className="text-xs bg-accent" variant="outline">Complete</Badge>
+                                    </TableCell>
+                                    <TableCell>05-05-2024</TableCell>
+                                </TableRow>
+                            </TableBody>
+                            </Table>
+                        </CardContent>
+                        </Card>
+                        <Card x-chunk="dashboard-01-chunk-5">
+                        <CardHeader>
+                            <CardTitle>Recent Activity</CardTitle>
+                        </CardHeader>
+                        <CardContent className="grid gap-8">
+                            <div className="flex items-center gap-4">
+                            <Avatar className="hidden h-9 w-9 sm:flex">
+                                <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                                <AvatarFallback>OM</AvatarFallback>
+                            </Avatar>
+                            <div className="grid gap-1">
+                                <p className="text-sm font-medium leading-none">
+                                NFT Upload
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                In Weekend Collection
+                                </p>
+                            </div>
+                            <div className="ml-auto text-sm font-medium">5/Oct/2024</div>
+                            </div>
+                            <div className="flex items-center gap-4">
+                            <Avatar className="hidden h-9 w-9 sm:flex">
+                                <AvatarImage src="/avatars/02.png" alt="Avatar" />
+                                <AvatarFallback>JL</AvatarFallback>
+                            </Avatar>
+                            <div className="grid gap-1">
+                                <p className="text-sm font-medium leading-none">
+                                Update Metadata
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                In Weekend Collection
+                                </p>
+                            </div>
+                            <div className="ml-auto text-sm font-medium">5/Oct/2024</div>
+                            </div>
+                            <div className="flex items-center gap-4">
+                            <Avatar className="hidden h-9 w-9 sm:flex">
+                                <AvatarImage src="/avatars/03.png" alt="Avatar" />
+                                <AvatarFallback>IN</AvatarFallback>
+                            </Avatar>
+                            <div className="grid gap-1">
+                                <p className="text-sm font-medium leading-none">
+                                Upload NFT
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                In Winter Collection
+                                </p>
+                            </div>
+                            <div className="ml-auto text-sm font-medium">23/Sep/2024</div>
+                            </div>
+                            <div className="flex items-center gap-4">
+                            <Avatar className="hidden h-9 w-9 sm:flex">
+                                <AvatarImage src="/avatars/04.png" alt="Avatar" />
+                                <AvatarFallback>WK</AvatarFallback>
+                            </Avatar>
+                            <div className="grid gap-1">
+                                <p className="text-sm font-medium leading-none">
+                                Upload NFT
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                In Summer Collection
+                                </p>
+                            </div>
+                            <div className="ml-auto text-sm font-medium">14/Sep/2024</div>
+                            </div>
+                            <div className="flex items-center gap-4">
+                            <Avatar className="hidden h-9 w-9 sm:flex">
+                                <AvatarImage src="/avatars/05.png" alt="Avatar" />
+                                <AvatarFallback>SD</AvatarFallback>
+                            </Avatar>
+                            <div className="grid gap-1">
+                                <p className="text-sm font-medium leading-none">
+                                Upload NFT
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                In Spring Collection
+                                </p>
+                            </div>
+                            <div className="ml-auto text-sm font-medium">23/Jun/2024</div>
+                            </div>
+                        </CardContent>
+                        </Card>
+                    </div>
+                </main>
+            </div>
+        </ProtectedRoute>
+    )
+  }
+  
--- frontend/src/components/Navigation/SideNav.tsx
+++ frontend/src/components/Navigation/SideNav.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link"
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import {
   Home,
   Heart,
+  HeartHandshake,
   List,
   Search,
   Upload,
   User
-} from "lucide-react"
-
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
+} from "lucide-react";
 
 export default function Navigation() {
+  const usePathName = usePathname();
   return (
     <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
@@ -37,48 +37,62 @@ export default function Navigation() {
               </Link>
           </div>
           <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4 lg:pt-8 ">
               <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 lg:pt-8 lg:py-5 text-muted-foreground transition-all hover:text-primary"
+                href="/dashboard"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/dashboard" ? "bg-muted text-primary" : ""}`}
               >
                 <Home className="h-6 w-6" />
                 Dashboard
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/upload" ? "bg-muted text-primary" : ""}`}
               >
                 <Upload className="h-6 w-6" />
                 Upload NFT
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 lg:py-5 text-primary transition-all hover:text-primary"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/view" ? "bg-muted text-primary" : ""}`}
               >
                 <List className="h-6 w-6" />
                 View NFTs
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/browse" ? "bg-muted text-primary" : ""}`}
               >
                 <Search className="h-6 w-6" />
                 Browse NFTs
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/favorite" ? "bg-muted text-primary" : ""}`}
               >
                 <Heart className="h-6 w-6" />
                 Favorites
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/profile" ? "bg-muted text-primary" : ""}`}
               >
                 <User className="h-6 w-6" />
                 Profile
+              </Link>
+              <Link
+                href="/contact"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 lg:py-5 text-muted-foreground transition-all hover:text-primary
+                ${usePathName === "/contact" ? "bg-muted text-primary" : ""}`}
+              >
+                <HeartHandshake className="h-6 w-6" />
+                Support
               </Link>
             </nav>
           </div>
